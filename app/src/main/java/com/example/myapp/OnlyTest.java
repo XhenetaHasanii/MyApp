22a5b822baf06c6190c8111fbbd2e04c774fa434
XhenetaHasanii <xheneta.hasanni@gmail.com>
2023-05-01 12:13:59 +0200

    update

---
+++ app/src/main/java/com/example/myapp/OnlyTest.java
@@ -0,0 +1,14 @@
+package com.example.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OnlyTest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_only_test);
+    }
+}
