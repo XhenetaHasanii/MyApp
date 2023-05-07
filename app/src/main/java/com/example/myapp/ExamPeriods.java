daa9ba8f2ee8f920a1a56cfed0d1b34e633abe9f
XhenetaHasanii <xheneta.hasanni@gmail.com>
2023-05-07 19:39:04 +0200

    Reformatted Code

--- app/src/main/java/com/example/myapp/ExamPeriods.java
+++ app/src/main/java/com/example/myapp/ExamPeriods.java
@@ -2,15 +2,12 @@ package com.example.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Dialog;
+
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.MenuItem;
+
 import android.view.View;
 import android.widget.Button;
-import android.widget.PopupMenu;
-import android.widget.Toast;
 
 public class ExamPeriods extends AppCompatActivity {
     @Override
