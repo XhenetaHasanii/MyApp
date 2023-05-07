cd2ad6e5c6f7d80851a57c06b9d4bc3729bc4963
XhenetaHasanii <xheneta.hasanni@gmail.com>
2023-05-07 10:29:29 +0200

    Added ListView

--- app/src/main/java/com/example/myapp/ItemList.java
+++ app/src/main/java/com/example/myapp/ItemList.java
@@ -3,27 +3,24 @@ package com.example.myapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Dialog;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.PopupWindow;
-import android.widget.TextView;
 
-public class Item_list extends AppCompatActivity {
+public class ItemList extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_list);
-        Button button = (Button) findViewById(R.id.button);
+        Button button=(Button)findViewById(R.id.subjects);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
                 public void onClick(View v) {
+                goToSubjects(v);
 
             }});}
-    public void bllablla(View view) {
+    public void goToSubjects(View view) {
         LayoutInflater inflater = LayoutInflater.from(this);
         View popupView = inflater.inflate(R.layout.activity_custom_pop_main, null);
         Dialog dialog = new Dialog(this);
@@ -31,8 +28,6 @@ public class Item_list extends AppCompatActivity {
 
         dialog.show();
         dialog.setTitle("Custom Pop-up");
-
-
 
 
 }
