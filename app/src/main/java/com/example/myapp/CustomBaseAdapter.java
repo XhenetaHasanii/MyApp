c3e28c5fdb8268b121af27d2b40e074123b46be6
XhenetaHasanii <xheneta.hasanni@gmail.com>
2023-05-12 16:08:53 +0200

    Added

--- app/src/main/java/com/example/myapp/CustomBaseAdapter.java
+++ app/src/main/java/com/example/myapp/CustomBaseAdapter.java
@@ -6,7 +6,6 @@ import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.BaseAdapter;
-import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -14,23 +13,23 @@ import androidx.core.content.ContextCompat;
 
 public class CustomBaseAdapter extends BaseAdapter {
 
-    ExamPeriodList listViewMain;
-    Animation animation1;
-    public String afatiIProvimeve[]={"Janar","Prill","Qershor","Shtator"};
+        ExamPeriodList listViewMain;
+        Animation animation1;
+        public String afatiIProvimeve[]={"Janar","Prill","Qershor","Shtator"};
 
-    public CustomBaseAdapter(ExamPeriodList listViewMain, String[] afatiIProvimeve) {
-        this.afatiIProvimeve=afatiIProvimeve;
-        this.listViewMain=listViewMain;
+        public CustomBaseAdapter(ExamPeriodList listViewMain, String[] afatiIProvimeve) {
+            this.afatiIProvimeve=afatiIProvimeve;
+            this.listViewMain=listViewMain;
 
 
 
-    }
-    public static int getRandom(int max){
-        return (int)(Math.random()*max);
-    }
+        }
+        public static int getRandom(int max){
+            return (int)(Math.random()*max);
+        }
 
-    @Override
-    public int getCount() {
+        @Override
+        public int getCount() {
 
             return afatiIProvimeve.length;}
 
@@ -38,46 +37,70 @@ public class CustomBaseAdapter extends BaseAdapter {
 
 
 
-    @Override
-    public Object getItem(int i) {
-        return i;
-    }
-
-    @Override
-    public long getItemId(int i) {
-        return i;
-    }
-
-    @Override
-    public View getView(int i, View view, ViewGroup viewGroup) {
-
-
-        view=LayoutInflater.from(listViewMain).inflate(R.layout.exam_periods,viewGroup,false);
-        animation1= AnimationUtils.loadAnimation(listViewMain,R.anim.animation1);
-        TextView textView=(TextView)view.findViewById(R.id.subjects);
-
-        LinearLayout ll_bg;
-        ll_bg=view.findViewById(R.id.ll_bg);
-        int number=getRandom(4);
-        if (number==1){
-            ll_bg.setBackground(ContextCompat.getDrawable(listViewMain,R.drawable.gradient_1));
+        @Override
+        public Object getItem(int i) {
+            return i;
         }
-        if (number==2){
-            ll_bg.setBackground(ContextCompat.getDrawable(listViewMain,R.drawable.gradient_2));
+
+        @Override
+        public long getItemId(int i) {
+            return i;
         }
-        if (number==3){
-            ll_bg.setBackground(ContextCompat.getDrawable(listViewMain,R.drawable.gradient_3));
-        }if (number==4){
-            ll_bg.setBackground(ContextCompat.getDrawable(listViewMain,R.drawable.gradient_4));
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            convertView= LayoutInflater.from(listViewMain).inflate(R.layout.exam_periods,parent,false);
+            animation1= AnimationUtils.loadAnimation(listViewMain,R.anim.animation1);
+            TextView textView=(TextView)convertView.findViewById(R.id.subjects);
+            LinearLayout ll_bg;
+            ll_bg=convertView.findViewById(R.id.ll_bg);
+            int number=getRandom(4);
+            if (number==1){
+                ll_bg.setBackground(ContextCompat.getDrawable(listViewMain,R.drawable.gradient_1));
+            }
+            if (number==2){
+                ll_bg.setBackground(ContextCompat.getDrawable(listViewMain,R.drawable.gradient_2));
+            }
+            if (number==3){
+                ll_bg.setBackground(ContextCompat.getDrawable(listViewMain,R.drawable.gradient_3));
+            }if (number==4){
+                ll_bg.setBackground(ContextCompat.getDrawable(listViewMain,R.drawable.gradient_4));
+            }
+            textView.setText(afatiIProvimeve[position]);
+            textView.setAnimation(animation1);
+            return convertView;
+
+        }}
+
+
+      /*  public View getView(int i, View view, ViewGroup viewGroup) {
+
+
+            view=LayoutInflater.from(listViewMain).inflate(R.layout.exam_periods,viewGroup,false);
+            animation1= AnimationUtils.loadAnimation(listViewMain,R.anim.animation1);
+            TextView textView=(TextView)view.findViewById(R.id.subjects);
+
+            LinearLayout ll_bg;
+            ll_bg=view.findViewById(R.id.ll_bg);
+            int number=getRandom(4);
+            if (number==1){
+                ll_bg.setBackground(ContextCompat.getDrawable(listViewMain,R.drawable.gradient_1));
+            }
+            if (number==2){
+                ll_bg.setBackground(ContextCompat.getDrawable(listViewMain,R.drawable.gradient_2));
+            }
+            if (number==3){
+                ll_bg.setBackground(ContextCompat.getDrawable(listViewMain,R.drawable.gradient_3));
+            }if (number==4){
+                ll_bg.setBackground(ContextCompat.getDrawable(listViewMain,R.drawable.gradient_4));
+            }
+            textView.setText(afatiIProvimeve[i]);
+            textView.setAnimation(animation1);
+
+
+            return view;
+
         }
-        textView.setText(afatiIProvimeve[i]);
-        textView.setAnimation(animation1);
-
-
-      return view;
-
-    }
-
-    }
+*/
 
 
--- app/src/main/java/com/example/myapp/ExamPeriodList.java
+++ app/src/main/java/com/example/myapp/ExamPeriodList.java
@@ -8,35 +8,57 @@ import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class ExamPeriodList extends AppCompatActivity {
+import java.util.ArrayList;
 
-    Animation animation1;
-    String afatiIProvimeve[]={"Janar","Prill","Qershor","Shtator"};
+public class ExamPeriodList extends AppCompatActivity {
+    ListView listView;
+    String month [] ={"Janar","Prill","Qershor","Shtator"};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.exam_period_list);
-        ListView listView=(ListView)findViewById(R.id.list_view);
-        animation1= AnimationUtils.loadAnimation(this,R.anim.animation1);
-        CustomBaseAdapter customBaseAdapter=new CustomBaseAdapter(ExamPeriodList.this,afatiIProvimeve);
-
-        listView.setAdapter(customBaseAdapter);
+        listView=(ListView)findViewById(R.id.list_view);
+        ArrayAdapter<String> arrayAdapter=new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line,month);
+        listView.setAdapter(arrayAdapter);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(ExamPeriodList.this,""+afatiIProvimeve[i],Toast.LENGTH_SHORT).show();
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                /*String select= (String) listView.getItemAtPosition(position);
+                Toast.makeText(MainActivity.this, ""+select, Toast.LENGTH_SHORT).show();*/
+                /*Intent intent=new Intent(MainActivity.this,ListSubjects.class);*/
+                /*intent.putExtra("itemPosition",position);*/
+                /* startActivity(intent);*/
+
+            }
+        });
+    }
+    private void showCustomPopup() {
+        final Dialog dialog = new Dialog(ExamPeriodList.this);
+        dialog.setContentView(R.layout.custom_popup);
+        dialog.setCancelable(true);
+
+        TextView textViewMessage = dialog.findViewById(R.id.textViewMessage);
+        Button buttonClose = dialog.findViewById(R.id.buttonClose);
+
+        textViewMessage.setText("This is a custom popup with text!");
+
+        buttonClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
             }
         });
 
-    }
-}
+        dialog.show();
+    }}
 
 
 
