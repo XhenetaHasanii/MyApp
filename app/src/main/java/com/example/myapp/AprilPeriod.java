b7fd140aefade22938d7304c5fe8b21892f0b4c2
XhenetaHasanii <xheneta.hasanni@gmail.com>
2023-05-15 10:38:17 +0200

    Added CustomPopup for each period

--- app/src/main/java/com/example/myapp/AprilPeriod.java
+++ app/src/main/java/com/example/myapp/AprilPeriod.java
@@ -2,11 +2,14 @@ package com.example.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -26,10 +29,28 @@ public class AprilPeriod extends AppCompatActivity {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent = new Intent(AprilPeriod.this, StudentProfile.class);
-                startActivity(intent);
+                showCustomPopup();
             }
         });
 
+    }
+    private void showCustomPopup() {
+        final Dialog dialog = new Dialog(AprilPeriod.this);
+        dialog.setContentView(R.layout.custom_popup);
+        dialog.setCancelable(true);
+
+        TextView textViewMessage = dialog.findViewById(R.id.textViewMessage);
+        Button buttonClose = dialog.findViewById(R.id.buttonClose);
+
+        textViewMessage.setText("Shume suksese ne provim!");
+
+        buttonClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+
+        dialog.show();
     }
 }
--- app/src/main/java/com/example/myapp/JunePeriod.java
+++ app/src/main/java/com/example/myapp/JunePeriod.java
@@ -2,10 +2,13 @@ package com.example.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -27,9 +30,28 @@ public class JunePeriod extends AppCompatActivity {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                System.out.println("Hello World");
+                showCustomPopup();
             }
         });
 
+    }
+    private void showCustomPopup() {
+        final Dialog dialog = new Dialog(JunePeriod.this);
+        dialog.setContentView(R.layout.custom_popup);
+        dialog.setCancelable(true);
+
+        TextView textViewMessage = dialog.findViewById(R.id.textViewMessage);
+        Button buttonClose = dialog.findViewById(R.id.buttonClose);
+
+        textViewMessage.setText("Shume suksese ne provim!");
+
+        buttonClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+
+        dialog.show();
     }
 }
