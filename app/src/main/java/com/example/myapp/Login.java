de64efb2e02faf20ddda23dcd3c74247af5c265e
XhenetaHasanii <xheneta.hasanni@gmail.com>
2023-05-22 16:11:01 +0200

    Implemented login with firebase authentication

--- app/src/main/java/com/example/myapp/Login.java
+++ app/src/main/java/com/example/myapp/Login.java
@@ -1,48 +1,84 @@
 package com.example.myapp;
 
+
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+
+import com.google.android.gms.tasks.OnCanceledListener;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+
 public class Login extends AppCompatActivity {
-    private EditText username;
-    private EditText password;
-    private Button login;
+
+    private static final String TAG = "EmailPassword";
+
+    private FirebaseAuth mAuth;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_login);
-        Button login = (Button) findViewById(R.id.loginBtn);
+        mAuth = FirebaseAuth.getInstance();
+
+
         EditText username = findViewById(R.id.username);
         EditText password = findViewById(R.id.password);
 
 
+
+
+      Button login=(Button)findViewById(R.id.loginBtn);
+
         login.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
                 String txt_username = username.getText().toString();
                 String txt_password = password.getText().toString();
-                if (TextUtils.isEmpty(txt_username) || TextUtils.isEmpty(txt_password)) {
-                    Toast.makeText(Login.this, "Empty Credentials", Toast.LENGTH_SHORT).show();
-                } else if (password.length() < 6) {
-                    Toast.makeText(Login.this, "Password too short", Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(Login.this, "Successfully", Toast.LENGTH_SHORT).show();
-                    Intent intent=new Intent(Login.this,Menu.class);
-                    startActivity(intent);
-                }
+                if (!TextUtils.isEmpty(txt_username) && !TextUtils.isEmpty(txt_password)) {
+                    mAuth.signInWithEmailAndPassword(txt_username,txt_password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                startActivity(new Intent(Login.this,ReadData.class));
+                                finish();
+                            }
+                            else {
+                                Toast.makeText(Login.this, "Something went wrong", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(Login.this, "Error", Toast.LENGTH_SHORT).show();
+                        }
+                    }).addOnCanceledListener(new OnCanceledListener() {
+                        @Override
+                        public void onCanceled() {
+                            Toast.makeText(Login.this, "Canceled", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+            }
+        }});
 
-        };
-    });
 
 
 
-}
-}
+
+
+}}
